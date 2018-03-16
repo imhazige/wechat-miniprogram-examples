9a4bb577e72bb3c59dcfa76c88c6c32304f3e827
KaZ <abc@a.com>
2018-03-16 18:06:31 +0800

    mpvue 地图测试

---
+++ mpvue-map/src/pages/index/index.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="container" @click="clickHandle('test click', $event)">
+
+    <button @click="onGoBubblesClick('test click', $event)">移动气泡</button>
+
+  </div>
+</template>
+
+<script>
+import card from "@/components/card";
+
+export default {
+  data() {
+    return {
+      motto: "Hello World",
+      userInfo: {}
+    };
+  },
+
+  components: {
+    card
+  },
+
+  methods: {
+    getUserInfo() {
+      // 调用登录接口
+      wx.login({
+        success: () => {
+          wx.getUserInfo({
+            success: res => {
+              this.userInfo = res.userInfo;
+            }
+          });
+        }
+      });
+    },
+    clickHandle(msg , ev) {
+      console.log("clickHandle:", msg, ev);
+    },
+    onGoBubblesClick(ev) {
+      const url = "/pages/movingBubble/movingBubble";
+      let arg = { url };
+      wx.navigateTo(arg);
+    }
+  },
+
+  created() {
+    // 调用应用实例的方法获取全局数据
+    this.getUserInfo();
+  }
+};
+</script>
+
+<style scoped>
+.userinfo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.userinfo-avatar {
+  width: 128rpx;
+  height: 128rpx;
+  margin: 20rpx;
+  border-radius: 50%;
+}
+
+.userinfo-nickname {
+  color: #aaa;
+}
+
+.usermotto {
+  margin-top: 150px;
+}
+
+.form-control {
+  display: block;
+  padding: 0 12px;
+  margin-bottom: 5px;
+  border: 1px solid #ccc;
+}
+</style>
---
+++ mpvue-map/src/pages/movingBubble/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <map></map>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      motto: "Hello World",
+      userInfo: {}
+    };
+  },
+
+  components: {},
+
+  methods: {
+    getUserInfo() {
+      // 调用登录接口
+      wx.login({
+        success: () => {
+          wx.getUserInfo({
+            success: res => {
+              this.userInfo = res.userInfo;
+            }
+          });
+        }
+      });
+    }
+  },
+
+  created() {}
+};
+</script>
+
+<style scoped>
+page{
+  height: 100%rpx;
+  width:100%rpx;
+}
+</style>
